f88a0a6b5071c1cdcbcc91706533c6aab779a3c3
adam <adamg42685@gmail.com>
2023-08-04 17:32:31 -0400

    initial commit threads

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,22 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import '../globals.css';
+export const metaData = {
+  title: "Threads",
+  description: "Threads is a simple forum app built with Next.js and Prisma.",
+};
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
